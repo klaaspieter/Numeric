dccc5388487586ec44304c4386a768c6ef3e51ea
Klaas Pieter Annema <klaaspieter@annema.me>
2014-10-08 12:53:28 +0200

    Add Test target

---
+++ Specs/BytesSpec.swift
@@ -0,0 +1,21 @@
+//
+//  Specs.swift
+//  Specs
+//
+//  Created by Klaas Pieter Annema on 08/10/14.
+//  Copyright (c) 2014 Klaas Pieter Annema. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+
+class BytesSpec: QuickSpec {
+    override func spec() {
+        describe("Bytes") {
+            it("fails") {
+                expect(true).to(beTruthy())
+            }
+        }
+    }
+}
